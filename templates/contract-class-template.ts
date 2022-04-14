49f6f6bbe7a06c1da02837c84bbf47b28bf9c7bf
roaminroe <roamin@tuta.io>
2022-04-14 13:50:51 -0400

    initial commit

--- templates/contract-class-template.ts
+++ templates/contract-class-template.ts
@@ -1,5 +1,5 @@
-import { authority } from 'koinos-as-sdk';
-import { ##_PROTO_PACKAGE_## } from "./proto/##_PROTO_PACKAGE_##";
+import * as authority from "../../koinos-proto-as/koinos/chain/authority";
+import * as ##_PROTO_PACKAGE_## from "./proto/##_PROTO_PACKAGE_##";
 
 export class ##_CONTRACT_NAME_## {
     ##_ENTRY_POINTS_##
--- templates/index-template.ts
+++ templates/index-template.ts
@@ -1,6 +1,7 @@
 import { System, Protobuf, authority } from 'koinos-as-sdk';
-import { ##_CONTRACT_NAME_## as ContractClass } from './##_CONTRACT_NAME_##';
-import { ##_PROTO_PACKAGE_## as ProtoNamespace } from "./proto/##_PROTO_PACKAGE_##";
+import * as authority from "../../koinos-proto-as/koinos/chain/authority";
+import * as ContractClass from './##_CONTRACT_NAME_##';
+import * as ProtoNamespace from "./proto/##_PROTO_PACKAGE_##";
 
 export function main(): i32 {
   const entryPoint = System.getEntryPoint();
